df47e7b3d2ecc736b42c739e6aba88fea3ae2ce3
Yeh-Chi-Chun <sam4628200@gmail.com>
2022-03-17 16:16:40 +0800

    新增panel 及 button

--- src/uml/App.java
+++ src/uml/App.java
@@ -1,4 +1,14 @@
 package uml;
 
+import javax.swing.*;
+
 public class App {
+    private JButton selectBtn;
+    private JPanel panelMain;
+    private JButton associationLineBtn;
+    private JButton generalizationLineBtn;
+    private JButton compositionLineBtn;
+    private JButton classBtn;
+    private JButton useCaseBtn;
+    private JPanel canvasPanel;
 }
